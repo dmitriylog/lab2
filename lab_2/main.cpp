30bff7a6c57e5310dac64e6fcab54e0e60e91877
d1m1d1n <dmitriy-loginov-02@mail.ru>
2025-09-17 15:35:17 +0300

    удаление старого(дублирующего алгоритма) + откомпилированная процедура создания файлов

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -5,7 +5,24 @@
 using namespace std;
 
 void creating_file(int i){
+    string fnames = "file" + to_string(i) + ".txt";
+    ofstream file(fnames);
 
+    if (!file.is_open()){
+        cerr << "Ошибка! Не удалось создать файл " << fnames << endl;
+        exit;
+    }
+
+    for (int j = 0; j < 10; j++) {
+        int random_num = rand() % 10 + 1;
+        file << random_num;
+
+        if (j < 9) {
+            file << " ";
+        }
+    }
+    file.close();
+    cout << "Файл " << fnames << " успешно создан." << endl;
 }
 
 int main()
